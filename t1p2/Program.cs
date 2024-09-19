2da9bb8696ac984a25d06009bcee114b8acc960a
Jeriko34 <goldplayer1207@gmail.com>
2024-09-19 09:04:24 -0500

    Primer Salvado

---
+++ t1p2/Program.cs
@@ -0,0 +1,28 @@
+﻿int placa = 0, conta = 1;
+string color;
+
+Console.WriteLine("Escriba los colores de la calcomania de los autos: ");
+color = Console.ReadLine().ToUpper();
+
+while (conta >= 1)
+{
+    if (color == "VERDE")
+    {
+        conta += 1;
+        for (placa += 1; placa <= 3 && placa >= 0;) { placa += 1; }     
+    }
+    else if (color == "AZUL")
+    {
+        for (placa += 1; placa <= 6 && placa > 3;) { placa += 1; }
+        conta = 1;
+    }
+   else if (color == "ROJO")
+    {
+        conta += 1;
+        for (placa += 1; placa <= 9 && placa > 6;) { placa += 1; }
+    }
+    else if (color == "F") {
+        conta -= 1;
+        Console.WriteLine($"{placa}:");
+    }
+}
---
+++ t1resuelta/Program.cs
@@ -0,0 +1,35 @@
+﻿string nombre;
+double vent = 0, salar = 0, comis = 0, des = 0, totgan = 0;
+int an = 0;
+Console.WriteLine("Escriba su nombre: ");
+nombre = Console.ReadLine();
+Console.WriteLine("Escriba su sueldo: ");
+salar = double.Parse(Console.ReadLine());
+Console.WriteLine("Ventas que se realizaron: ");
+vent = double.Parse(Console.ReadLine());
+Console.Write("Escriba los años que tiene la empresa: ");
+an = int.Parse(Console.ReadLine());
+if (vent < 600)
+{
+    comis = vent + (vent * 0.5);
+}
+else if (600 > vent && vent <= 3000)
+{
+    comis = vent + (vent * 0.07);
+}
+else
+    comis = 300 + (vent * 0.06);
+totgan = salar + comis;
+if (an >= 3)
+{
+    des = totgan * 0.065;
+}
+else
+    des = totgan * 0.045;
+Console.WriteLine($"Hola {nombre}");
+Console.WriteLine($"Su salario es {totgan}: ");
+Console.WriteLine($"Se te descuenta por impuestos: {des}: ");
+Console.WriteLine($"Salario neto: {totgan - des}: ");
+Console.ReadKey();
+
+
